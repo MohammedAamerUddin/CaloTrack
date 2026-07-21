cdff250a7c576b51dedc9b99c76316102ad8dee4
Mohammed Aameruddin <mdaameruddin01@gmail.com>
2026-07-22 03:28:23 +0530

    make some progess on home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './components/home-page/home-page';
+import { LoginPage } from './components/login-page/login-page';
+import { CreateAccountPage } from './components/create-account-page/create-account-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path :'',
+        component : HomePage,
+        title : 'Home Page'
+    },
+    {
+        path :'login',
+        component : LoginPage,
+        title : 'Login Page'
+    },
+    {
+        path :'create-account',
+        component : CreateAccountPage,
+        title : 'Create Account Page'
+    },
+    {
+        path :'**',
+        redirectTo : ''
+    }
+
+];
--- src/app/components/home-page/home-page.ts
+++ src/app/components/home-page/home-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Footer } from "../footer/footer";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [Footer],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
