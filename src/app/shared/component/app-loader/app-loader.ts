6a900c28ec95ca0d078b2c163bfcffa33e4e320d
Mohammed Aameruddin <mdaameruddin01@gmail.com>
2026-07-21 19:55:22 +0530

    App set up

---
+++ src/app/shared/component/app-loader/app-loader.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-loader',
+  imports: [],
+  templateUrl: './app-loader.html',
+  styleUrl: './app-loader.scss',
+})
+export class AppLoader {
+
+}
